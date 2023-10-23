4e098d7c73c2b855afc2fe6e95f7e9304be5f2ae
joshrizika <joshrizika@gmail.com>
2023-10-23 13:36:42 -0400

    cleaned up function, made dependant on one arg

--- display_data.py
+++ display_data.py
@@ -1,16 +1,30 @@
 import folium
 import pandas as pd
+from flight_data import findAirportCoordinatesByIATACode
 
-data = pd.read_csv("flightData/flight_log_BOS_10-20-23_12:10-13:10.csv", parse_dates=['timestamp'])
+data_path = "flightData/flight_log_BOS.csv"
 
-map = folium.Map(location=[42.3601, -71.0589], zoom_start=8)
+#function: to display all the points recorded in the collected data
+#parameters: data_path - string
+#returns: nothing
+def displayData(data_path):
+    iataCode = data_path[-7:-4] #get iata code from data path
 
-for index, row in data.iterrows():
-    tooltip_text = f"Timestamp: {row['timestamp']}<br>"
-    tooltip_text += f"Flight ICAO: {row['flight_icao']}<br>"
-    tooltip_text += f"Altitude: {row['alt']}<br>"
-    tooltip_text += f"Departure IATA: {row['dep_iata']}<br>"
-    tooltip_text += f"Arrival IATA: {row['arr_iata']}<br>"
-    folium.Marker([row['lat'], row['lng']], tooltip=tooltip_text).add_to(map)
-    
-map.save('map_boston.html')
+    data = pd.read_csv(data_path, parse_dates=['timestamp']) #create a dataframe using the data and set the dates to datetime type
+
+    map = folium.Map(location=list(findAirportCoordinatesByIATACode(iataCode)), zoom_start=8) #create the map with the airport at the center
+
+    for index, row in data.iterrows(): #for each marker add the data below
+        tooltip_text = f"Timestamp: {row['timestamp']}<br>"
+        tooltip_text += f"Flight ICAO: {row['flight_icao']}<br>"
+        tooltip_text += f"Latitude: {row['lat']}<br>"
+        tooltip_text += f"Longitude: {row['lng']}<br>"
+        tooltip_text += f"Altitude: {row['alt']}<br>"
+        tooltip_text += f"Departure IATA: {row['dep_iata']}<br>"
+        tooltip_text += f"Arrival IATA: {row['arr_iata']}<br>"
+        folium.Marker([row['lat'], row['lng']], tooltip=tooltip_text).add_to(map) #add the marker to the map
+        
+    map.save(f'flightData/maps/map_{iataCode}.html') #save the map
+
+if __name__ == "__main__":
+    displayData(data_path)
