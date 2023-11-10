3222d0e5cf198af6400fb0768044a891dcfc3419
joshrizika <joshrizika@gmail.com>
2023-11-10 12:54:25 -0500

    added comments

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from spots import getParks
 #parameters: line - dictionary
 #returns: a list of parks
 def getSpots(iataCode):
-    spots = set()
+    spots = set() #create empty set
 
-    flight_path_lines = getPaths(iataCode)
-    for line in flight_path_lines:
-        spots.update(getParks(line))
-    spots = list(spots)
-    # print(spots)
+    flight_path_lines = getPaths(iataCode) #get flight paths
+    for line in flight_path_lines: #for each flight path
+        spots.update(getParks(line)) #get parks and add them to spots set
+    spots = list(spots) #turn spots into a list
     return spots
 
 if __name__ == '__main__':
     spots = getSpots("DCA")
-    # print(spots)
-    # print(len(spots))
-
+    print(spots)
 
    
