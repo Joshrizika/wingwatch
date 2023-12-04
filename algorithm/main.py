60381b2031cdafe1249a85e29a5eb29b34ec3f87
joshrizika <joshrizika@gmail.com>
2023-12-04 17:11:13 -0500

    cleaned up code and added comments

--- algorithm/main.py
+++ algorithm/main.py
@@ -3,11 +3,11 @@ from spots import getParks
 import pandas as pd
 import json
 
-#This file runs the clustering and spot finding algorithms.
+#This file runs the clustering and spot finding algorithms and returns the data to a csv.
 
-#function: takes data from data_path, gets paths from data and finds parks along them
-#parameters: line - dictionary
-#returns: a list of parks
+#function: takes data from data_path, gets paths from data, finds parks along paths, and saves data to csv
+#parameters: iataCode - string
+#returns: nothing
 def getSpots(iataCode):
     spots = set() #create empty set
     flight_path_lines = getPaths(iataCode) #get flight paths
@@ -15,20 +15,11 @@ def getSpots(iataCode):
         spots.update(getParks(line)) #get parks and add them to spots set
     spots = list(spots) #turn spots into a list
 
-    # Convert each JSON string in the list to a dictionary
-    spots_data = [json.loads(spot) for spot in spots]
-
-    # Normalize nested JSON data and create DataFrame
-    df = pd.json_normalize(spots_data)
-
-   # Rename column 'displayName.text' to 'displayName'
-    df.rename(columns={'displayName.text': 'displayName', 'editorialSummary.text': 'editorialSummary', 'location.latitude': 'latitude', 'location.longitude': 'longitude'}, inplace=True)
-
-    # Remove the column 'displayName.languageCode'
-    df.drop(columns=['displayName.languageCode', 'editorialSummary.languageCode'], inplace=True)
-
-    # Save the DataFrame to a CSV file
-    df.to_csv(f'data/spotData/data/spots_{iataCode}.csv', index=False)
+    spots_data = [json.loads(spot) for spot in spots] #parse the JSON data
+    df = pd.json_normalize(spots_data) #create a DataFrame from the JSON data
+    df.rename(columns={'displayName.text': 'displayName', 'editorialSummary.text': 'editorialSummary', 'location.latitude': 'latitude', 'location.longitude': 'longitude'}, inplace=True) #rename columns
+    df.drop(columns=['displayName.languageCode', 'editorialSummary.languageCode'], inplace=True) #remove columns
+    df.to_csv(f'data/spotData/data/spots_{iataCode}.csv', index=False) #save data to .csv file
 
 if __name__ == '__main__':
     getSpots("BOS")
