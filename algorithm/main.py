2f921a85e6ed9e5bef4b38db1c14f2e06a5b6262
joshrizika <joshrizika@gmail.com>
2023-12-04 20:07:04 -0500

    implemented threading

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,6 +1,7 @@
 from clusters import getPaths
 from spots import getParks
 import pandas as pd
+import threading
 import json
 
 #This file runs the clustering and spot finding algorithms and returns the data to a csv.
@@ -21,7 +22,21 @@ def getSpots(iataCode):
     df.drop(columns=['displayName.languageCode', 'editorialSummary.languageCode'], inplace=True) #remove columns
     df.to_csv(f'data/spotData/data/spots_{iataCode}.csv', index=False) #save data to .csv file
 
+#function: finds spots for all inputted airports simultaneously
+#parameters: iataCodes - list
+#returns: nothing
+def getSpotsThreading(iataCodes): #DISABLE ALL FOLIUM AND MATPLOTLIB PLOTS WHEN USING THIS FUNCTION
+    threads = [] #initialize empty threads list
+    for iataCode in iataCodes: #for each airport
+        thread = threading.Thread(target=getSpots, args=(iataCode,)) #initialize a thread to call getSpots on the iataCode
+        thread.start() #start the thread
+        threads.append(thread) #append to list of threads
+
+    for thread in threads: #for each thread
+        thread.join() #make sure that the main program waits until thread is finished
+
 if __name__ == '__main__':
-    getSpots("BOS")
+    iataCodes = ['ATL', 'BOS', 'DCA', 'DEN', 'DFW', 'JFK', 'EWR', 'IAD', 'JFK', 'LAX', 'LGA', 'ORD', 'PHL']
+    getSpotsThreading(iataCodes)
 
    
