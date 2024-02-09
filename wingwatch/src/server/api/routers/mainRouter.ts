480121a1ab04659e6eeda22e7daf1f2d522909f2
joshrizika <joshrizika@gmail.com>
2024-02-09 14:08:27 -0500

    Add new API endpoints for finding top places and closest places

--- wingwatch/src/server/api/routers/mainRouter.ts
+++ wingwatch/src/server/api/routers/mainRouter.ts
@@ -16,6 +16,32 @@ export const mainRouter = createTRPCRouter({
       const places = await db.places.findMany();
       return places;
     }),
+  findTopPlaces: publicProcedure
+    .input(z.void()) // No input required for this procedure
+    .query(async () => {
+      const places = await db.places.findMany();
+
+      // Perform the calculation and sort the results
+      const sortedPlaces = places.map(place => ({
+        ...place,
+        value: Math.sqrt(Math.pow(place.distance_from_flightpath, 2) + Math.pow(place.average_altitude, 2))
+      })).sort((a, b) => a.value - b.value);
+
+      return sortedPlaces;
+    }),
+  findClosestPlaces: publicProcedure
+    .input(z.object({ latitude: z.number(), longitude: z.number() })) // Input required for this procedure
+    .query(async ({ input }) => {
+      const places = await db.places.findMany();
+
+      // Perform the calculation and sort the results
+      const sortedPlaces = places.map(place => ({
+        ...place,
+        value: Math.sqrt(Math.pow(place.latitude - input.latitude, 2) + Math.pow(place.longitude - input.longitude, 2))
+      })).sort((a, b) => a.value - b.value);
+
+      return sortedPlaces;
+    }),
   findPaths: publicProcedure
     .input(z.void()) // No input required for this procedure
     .query(async () => {
