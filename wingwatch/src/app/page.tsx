6c7030109cf2d3187a5ec2fbe280ca465389ae85
joshrizika <joshrizika@gmail.com>
2024-02-18 10:57:43 -0500

    Subject: Fix syntax and streamline homepage layout
    Corrected a missing semicolon in the use client directive and
    restructured the main tag's class ordering for consistency. These
    changes enhance readability and maintain coding standards, ensuring
    smoother future development and adherence to project style guidelines.
    Additionally, unnecessary whitespace and an obsolete variable
    declaration were removed, streamlining the codebase without altering
    functionality.
    
    No related issues.

--- wingwatch/src/app/page.tsx
+++ wingwatch/src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { unstable_noStore as noStore } from "next/cache";
 import { api } from "~/trpc/react";
@@ -10,13 +10,11 @@ export default function Home() {
   const session = api.main.getSession.useQuery().data;
 
   // const { location, error } = useLocation();
-  
-  
 
   return (
     <>
       <Navbar />
-      <main className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
         <div className="text-center">
           <p className="text-2xl text-black">
             {session && <span>Logged in as {session.user?.name}</span>}
@@ -25,4 +23,4 @@ export default function Home() {
       </main>
     </>
   );
-      }
+}
