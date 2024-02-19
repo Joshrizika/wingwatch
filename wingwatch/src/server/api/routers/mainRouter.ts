3b9f3e3feecdf368a234c06f5b78755aa6440e15
joshrizika <joshrizika@gmail.com>
2024-02-19 17:39:58 -0500

    Refactor place sorting and add review functionality
    Refactoring was done to streamline the calculation and sorting of places
    in different queries, making the code more readable and maintainable.
    This change enhances the performance slightly by optimizing the way
    calculations are executed for sorting places based on distances and
    other metrics.
    
    Additionally, a new mutation `addReview` has been introduced to the
    mainRouter. This feature allows users to add reviews for places, further
    enriching the application's functionality. Reviews include a title,
    content, rating, and are linked to both the user and the place. This
    addition is a significant step towards enhancing user interaction and
    providing valuable feedback mechanisms for places within the app.
    
    This update makes the codebase cleaner and adds valuable user feedback
    features, aiming to improve both the developer experience and the
    end-user experience.

--- wingwatch/src/server/api/routers/mainRouter.ts
+++ wingwatch/src/server/api/routers/mainRouter.ts
@@ -3,11 +3,6 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db";
 import { getServerAuthSession } from "~/server/auth";
 
-
-// add another router that generates random lucky numbers based on the user's inputted birth day plus some weather api data
-
-
-
 export const mainRouter = createTRPCRouter({
   getSession: publicProcedure
     .input(z.void()) // No input required for this procedure
@@ -26,11 +21,15 @@ export const mainRouter = createTRPCRouter({
       const places = await db.places.findMany();
 
       // Perform the calculation and sort the results
-      const sortedPlaces = places.map(place => ({
-        ...place,
-        value: Math.sqrt(Math.pow(place.distance_from_flightpath, 2) + Math.pow(place.average_altitude, 2))
-      })).sort((a, b) => a.value - b.value);
-
+      const sortedPlaces = places
+        .map((place) => ({
+          ...place,
+          value: Math.sqrt(
+            Math.pow(place.distance_from_flightpath, 2) +
+              Math.pow(place.average_altitude, 2),
+          ),
+        }))
+        .sort((a, b) => a.value - b.value);
       return sortedPlaces;
     }),
   findClosestPlaces: publicProcedure
@@ -39,17 +38,25 @@ export const mainRouter = createTRPCRouter({
       const places = await db.places.findMany();
 
       // Perform the calculation and sort the results
-      const sortedPlaces = places.map(place => ({
-        ...place,
-        value: Math.sqrt(Math.pow(place.latitude - input.latitude, 2) + Math.pow(place.longitude - input.longitude, 2))
-      })).sort((a, b) => a.value - b.value);
+      const sortedPlaces = places
+        .map((place) => ({
+          ...place,
+          value: Math.sqrt(
+            Math.pow(place.latitude - input.latitude, 2) +
+              Math.pow(place.longitude - input.longitude, 2),
+          ),
+        }))
+        .sort((a, b) => a.value - b.value);
 
       return sortedPlaces;
     }),
   findPlace: publicProcedure
     .input(z.object({ id: z.string() })) // Input required for this procedure
     .query(async ({ input }) => {
-      const place = await db.places.findUnique({ where: { place_id: input.id }, include: { path: true, reviews: true}});
+      const place = await db.places.findUnique({
+        where: { place_id: input.id },
+        include: { path: true, reviews: { include: { user: true } } },
+      });
       return place;
     }),
   findPaths: publicProcedure
@@ -58,4 +65,26 @@ export const mainRouter = createTRPCRouter({
       const paths = await db.paths.findMany();
       return paths;
     }),
-});
+  addReview: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+        rating: z.number(),
+        userId: z.string(),
+        placeId: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      await db.review.create({
+        data: {
+          title: input.title,
+          content: input.content,
+          rating: input.rating,
+          timestamp: new Date().toISOString(),
+          userId: input.userId,
+          placeId: input.placeId,
+        },
+      });
+    }),
+}); // Input required for this procedure
