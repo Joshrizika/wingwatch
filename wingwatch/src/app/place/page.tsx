d6f35102f2024d38dd94fc5403836bcfe9d44882
joshrizika <joshrizika@gmail.com>
2024-02-19 17:38:45 -0500

    Refactor place page and enhance review system
    - Simplified the RatingBar component by removing inline SVG logic in
    favor of a separate, reusable component across the app, improving
    maintainability and readability.
    - Introduced the Review component and modal, enabling users to submit
    reviews directly from the place page, thereby enhancing user interaction
    and content richness.
    - Removed unnecessary code and comments, streamlining the page's
    structure and improving performance by avoiding redundant calculations
    and database queries.
    - Updated the mainRouter backend logic to include user data in reviews
    and added the ability for users to submit reviews, making the review
    system more informative and interactive.
    - Employed useState hook for managing the review modal state, enriching
    the UI with dynamic content presentation.
    
    These changes aim to make the place pages more engaging and informative
    by seamlessly integrating user reviews and simplifying the UI
    components' structure, resulting in a more concise and readable
    codebase.
    
    Resolves #42

--- wingwatch/src/app/place/page.tsx
+++ wingwatch/src/app/place/page.tsx
@@ -2,79 +2,17 @@
 
 import { useEffect } from "react";
 import Navbar from "../_components/Navbar";
+import RatingBar from "../_components/RatingBar";
+import Review from "../_components/Review";
 import { useSearchParams } from "next/navigation";
 import { api } from "~/trpc/react";
-import { unstable_noStore as noStore } from "next/cache";
-
-const RatingBarWithStars = ({ rating }: { rating: number }) => {
-  // Calculate the percentage of the bar that should be filled based on the rating
-  const fillPercentage = (rating / 5) * 100;
-
-  // Adjust the SVG width and viewBox if necessary to fully display all stars
-  return (
-    <div
-      style={{
-        position: "relative",
-        width: "260px",
-        height: "50px",
-        backgroundColor: "transparent",
-      }}
-    >
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 260 50"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Define the star shape */}
-        <defs>
-          <clipPath id="star-mask">
-            {/* Repeated star paths */}
-            <path
-              d="M12 .587l3.668 7.431 8.332 1.209-6.035 5.884 1.425 8.309-7.39-3.884-7.39 3.884 1.425-8.309-6.035-5.884 8.332-1.209L12 .587z"
-              transform="scale(2.0833333)"
-            />
-            <path
-              d="M37 .587l3.668 7.431 8.332 1.209-6.035 5.884 1.425 8.309-7.39-3.884-7.39 3.884 1.425-8.309-6.035-5.884 8.332-1.209L37 .587z"
-              transform="scale(2.0833333)"
-            />
-            <path
-              d="M62 .587l3.668 7.431 8.332 1.209-6.035 5.884 1.425 8.309-7.39-3.884-7.39 3.884 1.425-8.309-6.035-5.884 8.332-1.209L62 .587z"
-              transform="scale(2.0833333)"
-            />
-            <path
-              d="M87 .587l3.668 7.431 8.332 1.209-6.035 5.884 1.425 8.309-7.39-3.884-7.39 3.884 1.425-8.309-6.035-5.884 8.332-1.209L87 .587z"
-              transform="scale(2.0833333)"
-            />
-            <path
-              d="M112 .587l3.668 7.431 8.332 1.209-6.035 5.884 1.425 8.309-7.39-3.884-7.39 3.884 1.425-8.309-6.035-5.884 8.332-1.209L112 .587z"
-              transform="scale(2.0833333)"
-            />
-          </clipPath>
-        </defs>
-        {/* Use the star shape as a mask */}
-        <rect
-          width="100%"
-          height="100%"
-          fill="transparent"
-          clip-path="url(#star-mask)"
-        />
-        {/* The fill level bar, adjust the fill color to match your design */}
-        <rect
-          width={`${fillPercentage}%`}
-          height="100%"
-          fill="#f59e0b"
-          clip-path="url(#star-mask)"
-        />
-      </svg>
-    </div>
-  );
-};
+// import Link from "next/link";
+import { format } from "date-fns";
+import { useState } from "react";
 
 export default function Place() {
-  noStore();
-
   const id = useSearchParams().get("id");
+  const session = api.main.getSession.useQuery().data;
   const placeQuery = api.main.findPlace.useQuery({ id: id! });
 
   const reviews = placeQuery.data?.reviews;
@@ -88,6 +26,11 @@ export default function Place() {
   } else {
     totalRating = 0;
   }
+
+  const [isReviewModalOpen, setIsReviewModalOpen] = useState(false);
+  const toggleReviewModal = () => {
+    setIsReviewModalOpen(!isReviewModalOpen);
+  };
 
   useEffect(() => {
     // Dynamically load the Google Maps script with the initMap callback
@@ -142,38 +85,93 @@ export default function Place() {
   return (
     <>
       <Navbar />
-      <div className="mt-5 flex min-h-screen">
-        <div className="flex-1 p-5">
-          <div className="text-center">
-            <h1 className="text-xl font-bold">{placeQuery.data?.name}</h1>
-            {placeQuery.data?.description && (
-              <p className="mt-2">{placeQuery.data?.description}</p>
-            )}
-          </div>
-          <p className="mt-2">Located at: {placeQuery.data?.address}</p>
-          <p className="mt-2">Airport: {placeQuery.data?.airport}</p>
-          <p className="mt-2">
-            Distance from the Flightpath:{" "}
-            {placeQuery.data?.distance_from_flightpath}
-          </p>
-          <p className="mt-2">
-            Average Altitude: {placeQuery.data?.average_altitude}
-          </p>
-          <p className="mt-2">
-            Distance from Airport: {placeQuery.data?.distance_from_airport}
-          </p>
-
-          {/* Gap and Rating section with header */}
-          <div className="mt-8 text-center">
-            <h2 className="mb-4 text-2xl font-bold">Rating</h2>
-            <div className="flex justify-center">
-              <RatingBarWithStars rating={totalRating} />
+      <div className="mt-5 flex min-h-screen flex-col">
+        <div className="flex flex-wrap md:flex-nowrap">
+          <div className="flex-1 p-5">
+            <div className="text-center">
+              <h1 className="text-xl font-bold">{placeQuery.data?.name}</h1>
+              {placeQuery.data?.description && (
+                <p className="mt-2">{placeQuery.data?.description}</p>
+              )}
             </div>
+            <p className="mt-2">Located at: {placeQuery.data?.address}</p>
+            <p className="mt-2">Airport: {placeQuery.data?.airport}</p>
+            <p className="mt-2">
+              Distance from the Flightpath:{" "}
+              {placeQuery.data?.distance_from_flightpath}
+            </p>
+            <p className="mt-2">
+              Average Altitude: {placeQuery.data?.average_altitude}
+            </p>
+            <p className="mt-2">
+              Distance from Airport: {placeQuery.data?.distance_from_airport}
+            </p>
+
+            {/* Gap and Rating section with header */}
+            <div className="mt-8 text-center">
+              <h2 className="mb-4 text-2xl font-bold">Rating</h2>
+              <div className="flex justify-center">
+                <RatingBar rating={totalRating} />
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full md:w-1/2">
+            <div id="map" className="h-full w-full"></div>
           </div>
         </div>
 
-        <div className="w-full md:h-auto md:w-1/2">
-          <div id="map" className="h-1/2 w-full"></div>
+        {/* Review Section moved to ensure it's below all content */}
+        <div className="mt-10 w-full">
+          {session && (
+            <div className="mb-4 flex justify-start">
+              {/* Replace the Link and its child button with this button */}
+              <button
+                className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                onClick={toggleReviewModal}
+              >
+                Add Review
+              </button>
+            </div>
+          )}
+
+          {/* Modal display logic */}
+          {isReviewModalOpen && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <Review onClose={toggleReviewModal} />
+            </div>
+          )}
+          <h2 className="mb-4 text-2xl font-bold">Reviews</h2>
+          {reviews && reviews.length > 0 ? (
+            reviews.map((review) => (
+              <div
+                key={review.id}
+                className="mb-4 rounded-lg border p-4 shadow-lg"
+              >
+                <div className="flex">
+                  <div className="mr-4">
+                    <RatingBar rating={review.rating} />
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-semibold">
+                      {review.title ?? "No Title"}
+                    </h3>
+                    <p className="text-gray-700">
+                      {review.content ?? "No Content"}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">
+                      User: {review.user?.name ?? "Unknown"}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">
+                      Date: {format(new Date(review.timestamp), "PP")}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No reviews yet.</p>
+          )}
         </div>
       </div>
     </>
