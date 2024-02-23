008e05fe25699242027be3cdb6a407e723bdb384
joshrizika <joshrizika@gmail.com>
2024-02-22 19:11:57 -0500

    fixed review pop up

--- wingwatch/src/app/_components/Review.tsx
+++ wingwatch/src/app/_components/Review.tsx
@@ -24,7 +24,7 @@ export default function Review({ onClose }: ReviewProps) {
     // Check if the current user has already submitted a review
     if (session?.user?.id) {
       const userReview = placeQuery.data?.reviews?.find(
-        (review) => review.userId === session.user.id
+        (review) => review.userId === session.user.id,
       );
       setHasReviewed(!!userReview);
     }
@@ -50,50 +50,57 @@ export default function Review({ onClose }: ReviewProps) {
             onClose();
             void placeQuery.refetch();
           },
-        }
+        },
       );
     }
   };
 
   return (
     <>
-      <div className="mt-5 min-w-[300px] rounded bg-white p-5 shadow-lg">
-        <div className="flex justify-end">
-          <button onClick={onClose} className="text-lg font-bold">
-            &times;
-          </button>
-        </div>
-        <h2 className="text-lg font-semibold">Leave a Review</h2>
-        <form className="w-full max-w-md" onSubmit={handleSubmit}>
-          <div>
-            <InteractiveRatingBar onRatingSelected={handleRatingSelect} />
+      <div
+        className="z-15 fixed inset-0 bg-black bg-opacity-50"
+        onClick={onClose}
+      >
+        <div className="fixed inset-0 z-20 flex items-center justify-center">
+          <div className="mt-5 min-w-[300px] rounded bg-white p-5 shadow-lg">
+            <div className="flex justify-end">
+              <button onClick={onClose} className="text-lg font-bold">
+                &times;
+              </button>
+            </div>
+            <h2 className="text-lg font-semibold">Leave a Review</h2>
+            <form className="w-full max-w-md" onSubmit={handleSubmit}>
+              <div>
+                <InteractiveRatingBar onRatingSelected={handleRatingSelect} />
+              </div>
+              <input
+                className="my-2 w-full border p-2"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <textarea
+                className="my-2 w-full border p-2"
+                rows={4}
+                placeholder="Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+              {session && !hasReviewed ? (
+                <button
+                  type="submit"
+                  className="mt-4 w-full rounded bg-blue-500 py-2 text-white"
+                >
+                  Submit Review
+                </button>
+              ) : (
+                <p className="mt-4 text-red-500">
+                  You have already submitted a review for this place.
+                </p>
+              )}
+            </form>
           </div>
-          <input
-            className="my-2 w-full border p-2"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <textarea
-            className="my-2 w-full border p-2"
-            rows={4}
-            placeholder="Content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          {session && !hasReviewed ? (
-            <button
-              type="submit"
-              className="mt-4 w-full rounded bg-blue-500 py-2 text-white"
-            >
-              Submit Review
-            </button>
-          ) : (
-            <p className="mt-4 text-red-500">
-              You have already submitted a review for this place.
-            </p>
-          )}
-        </form>
+        </div>
       </div>
     </>
   );
