271d3e5a535185dcea492134d18d229f1e84815f
joshrizika <joshrizika@gmail.com>
2024-03-04 15:26:54 -0500

    added local storage for settings on explore page

--- wingwatch/src/app/explore/page.tsx
+++ wingwatch/src/app/explore/page.tsx
@@ -19,10 +19,15 @@ export default function Explore() {
   const placesQuery = api.main.findPlaces.useQuery();
   const pathsQuery = api.main.findPaths.useQuery();
   const [location, setLocation] = useState<ILocation | undefined>(undefined);
-  const [radius, setRadius] = useState(30);
+
+  const [radius, setRadius] = useState(
+    Number(localStorage.getItem('radius')) || 30
+  );
   const [tempRadius, setTempRadius] = useState(radius); // Temporary radius state
   const sliderRef = useRef(null); // Ref for the slider element
-  const [sortOption, setSortOption] = useState("best");
+  const [sortOption, setSortOption] = useState(
+    localStorage.getItem('sortOption') ?? "best"
+  );
 
   // Adjust handleSliderChange to update the radius immediately
   const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,11 +37,13 @@ export default function Explore() {
   // Update radius when the user stops interacting with the slider
   const handleSliderChangeComplete = () => {
     setRadius(tempRadius);
+    localStorage.setItem('radius', String(tempRadius));
   };
 
   // Handle sort option change
   const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSortOption(e.target.value);
+    localStorage.setItem('sortOption', e.target.value);
   };
 
   useEffect(() => {
