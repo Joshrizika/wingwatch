4e19abbca393e3a7237ebb31f8647e121e2c6738
joshrizika <joshrizika@gmail.com>
2024-03-09 18:07:38 -0500

    added contribute page

---
+++ wingwatch/src/app/contribute/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Navbar from "../_components/Navbar";
+
+export default function Contribute() {
+  // Wrap the component or the specific logic that requires useSearchParams within Suspense
+  return (
+    <>
+      <Navbar />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        Contribute page to be added
+      </div>
+    </>
+  );
+}
