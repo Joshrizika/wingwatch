9bca787fe1014be9b4727b2d0b883585def547b2
joshrizika <joshrizika@gmail.com>
2024-03-15 21:33:35 -0400

    removed commented out code

--- wingwatch/src/app/page.tsx
+++ wingwatch/src/app/page.tsx
@@ -3,13 +3,10 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { api } from "~/trpc/react";
 import Navbar from "./_components/Navbar";
-// import useLocation from "./hooks/useLocation";
 
 export default function Home() {
   noStore();
   const session = api.main.getSession.useQuery().data;
-
-  // const { location, error } = useLocation();
 
   return (
     <>
