2dd4768fe2ba87d86d04fbc90c221b5b5cc2f708
joshrizika <joshrizika@gmail.com>
2024-06-12 22:08:59 -0400

    added all airports and changed airport ids

--- algorithm/database/airport_upload.py
+++ algorithm/database/airport_upload.py
@@ -1,10 +1,22 @@
 import psycopg2
 import re
 import json
+from cuid2 import cuid_wrapper
+from typing import Callable
 
-# Database connection details
+# Generate a unique identifier for the path.
+cuid_generator: Callable[[], str] = cuid_wrapper()
+
+# Development Database connection details
+# DATABASE = "wing-watch-db"
+# HOST = "localhost"
+# USER = "postgres"
+# PASSWORD = ""
+# PORT = "5432"
+
+# Production Database connection details
 DATABASE = "defaultdb"
-HOST = "wingwatch-do-user-15288104-0.c.db.ondigitalocean.com"
+HOST = "wingwatch-prod-do-user-15288104-0.c.db.ondigitalocean.com"
 USER = "doadmin"
 PASSWORD = "AVNS_nwxac_LLHhPIG0DV_4Y"
 PORT = "25060"
@@ -21,7 +33,7 @@ DROP TABLE IF EXISTS airports CASCADE;
 # Schema to create the airports table
 create_table_query = """
 CREATE TABLE IF NOT EXISTS airports (
-    airport_id SERIAL PRIMARY KEY,
+    airport_id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     iata_code CHAR(3) NOT NULL,
     latitude DOUBLE PRECISION NOT NULL,
@@ -43,11 +55,12 @@ def load_airport_data(js_file_path):
 # Function to insert airport data into the database
 def insert_airport_data(airport_data):
     insert_query = """
-    INSERT INTO airports (name, iata_code, latitude, longitude, elevation)
-    VALUES (%s, %s, %s, %s, %s);
+    INSERT INTO airports (airport_id, name, iata_code, latitude, longitude, elevation)
+    VALUES (%s, %s, %s, %s, %s, %s);
     """
     for airport in airport_data:
-        if airport["type"] == "large_airport":  # Ensure the airport is a large airport
+        if airport["type"] != "large_airport":  # Include all airports except large airports
+            print(airport["name"])
             # Splitting coordinates into latitude and longitude
             coordinates = airport["coordinates"].split(", ")
             latitude = float(coordinates[1])
@@ -57,16 +70,25 @@ def insert_airport_data(airport_data):
             # Extract name and IATA code
             name = airport["name"]
             iata_code = airport["iata_code"]
-            # Execute the insert query
-            cursor.execute(insert_query, (name, iata_code, latitude, longitude, elevation))
+            # Attempt to execute the insert query
+            try:
+                cursor.execute(insert_query, (cuid_generator(), name, iata_code, latitude, longitude, elevation))
+                connection.commit()
+            except psycopg2.Error as e:
+                connection.rollback()
+                if "duplicate key value violates unique constraint" in str(e):  
+                    print(f"Skipped duplicate airport with IATA code: {iata_code}")
+                else:
+                    print(f"An error occurred: {e}")
+                    raise e
 
 try:
-    cursor.execute(drop_table_query)  # Drop the existing table if it exists
-    cursor.execute(create_table_query)  # Create a new table
+    # cursor.execute(drop_table_query)  # Drop the existing table if it exists
+    # cursor.execute(create_table_query)  # Create a new table
     airport_data = load_airport_data("data/airport_data.js")
     insert_airport_data(airport_data)
     connection.commit()
-    print("Airports table created and data inserted successfully for large airports only.")
+    print("Airports table created and data inserted successfully for medium and small airports only.")
 except psycopg2.Error as e:
     print(f"An error occurred: {e}")
     connection.rollback()
